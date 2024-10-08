1ca07faf2b4e83974725f960ccc12a9c81713d37
Legend373 <164746195+Legend373@users.noreply.github.com>
2024-10-08 18:05:44 -0300

    first commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes, } from 'react-router-dom'
+import Home from './Pages/Home';
+import Collection from './Pages/Collection';
+import Contact from './Pages/Contact';
+import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import Orders from './Pages/Orders';
+import PlaceOrder from './Pages/PlaceOrder';
+import Product from './Pages/Product';
+import About from './Pages/About';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] 1g:px-[9vw] '>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
